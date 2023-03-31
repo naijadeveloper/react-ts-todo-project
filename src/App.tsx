9f2dc86ec5e9df127f4040f1a157382d7e9dc355
Enoch <mmejuenoch@gmail.com>
2023-03-31 13:34:33 +0100

    Installed tailwindcss for the styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 // import { useState } from 'react'
 
-function App() {
+const App:React.FC = () => {
   return (
-    <div className="App">
-      Hello Typescript
+    <div className="App w-screen h-screen flex flex-col items-center bg-blue-500">
+      <span className="heading font-neu uppercase text-4xl my-4 mx-0 text-white text-center z-[1] md:text-5xl md:my-[30px]">Taskify</span>
     </div>
   )
 }
